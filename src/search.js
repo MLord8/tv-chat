2ac0407f8cbaafd4c51a4c0c6efb5da6707498bd
nikhilramesh <nikhilramesh1@gmail.com>
2016-09-24 17:14:51 -0400

    search.js stuff

--- src/search.js
+++ src/search.js
@@ -1,4 +1,37 @@
-		$("#searchmodule").add()
+var apikey = "5ef2f92f2125441fb64a9324a42832af";
+
+function populateAutoSuggList() {
+	var json = $(function() {
+		var params = {
+            // Request parameters
+            "q": $("#search").val(),
+        };
+      
+        $.ajax({
+            url: "https://api.cognitive.microsoft.com/bing/v5.0/suggestions/?" + $.param(params),
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", apikey);
+            },
+            type: "GET",
+            // Request body
+            data: "{body}",
+        })
+        .done(function(data) {
+            alert("success");
+        })
+        .fail(function() {
+            alert("error");
+        });
+    });
+
+    var obj = JSON.parse(text);
+    console.log(obj);
+}
+
+var main = function() {
+	$("#search").change(function() {
+		populateAutoSuggList();
 	})
 }
 
