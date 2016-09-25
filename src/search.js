e4caa9b2ef607e4d1176a94389b80e12a5c81a3e
nikhilramesh <nikhilramesh1@gmail.com>
2016-09-25 05:20:22 -0400

    UI changes

--- src/search.js
+++ src/search.js
@@ -4,12 +4,12 @@ var json;
 
 
 function populateAutoSuggList(input, callback) {
-	
+
 		var params = {
             // Request parameters
             "q": input,
         };
-      	
+
         $.ajax({
             url: "https://api.cognitive.microsoft.com/bing/v5.0/suggestions/?" + "q=" + input,
             beforeSend: function(xhrObj){
@@ -31,7 +31,7 @@ function populateAutoSuggList(input, callback) {
 			callback();
         })
         .fail(function() {
-        });	
+        });
 }
 
 function buildList() {
@@ -54,7 +54,7 @@ function cardList(shows) {
     );*/
     var $parent = $('<div class="card card-block" style="width: 60rem;"></div>');
 
-    var $h3 = $('<h3 class="card-title">'+shows.MainTitle+'</h3>');
+    var $h3 = $('<h3 class="card-title">'+shows.MainTitle+"&emsp;"+shows.AirTime+'</h3>');
     //$h3.addClass("card-title");
     //$h3.text(shows.MainTitle);
 
@@ -66,10 +66,9 @@ function cardList(shows) {
     //$p2.addClass("card-text");
     //$p2.addText(shows.Description);
 
-    var $a = $('<a href="#" class="btn btn-primary">' + "Go to this chat!" + '</a>');
+    var $a = $('<a href="#" class="btn btn-primary" id='+shows.MainTitle+'>' + "Go to this chat!" + '</a>');
     //$a.addClass("btn btn-primary");
     //$a.addText("Go to this chat!");
-
     $parent.append($h3);
     $parent.append($p1);
     $parent.append($p2);
@@ -108,7 +107,7 @@ var shows = [
   {
     "MainTitle": "Breaking Bad",
     "EpisodeName": "Pilot",
-    "Description": "Walter White, a 50-year-old chemistry teacher, secretly begins making crystallized methamphetamine to support his family after learning that he has terminal lung cancer. He teams up with a former student, Jesse Pinkman, who is a meth dealer. Jesse tries to sell the meth they made, but the dealers snatch him and make him show them their lab, which is in an old RV. Walt knows the dealers intend to kill him, so he poisons them while pretending to share his recipe.",
+    "Description": "Diagnosed with terminal lung cancer, chemistry teacher Walter White teams up with a former student to cook and sell crystal meth.",
     "AirTime": "9:30 pm",
     "DateofAir": "09-24-16",
     "Runtime": "60 minutes"
@@ -116,7 +115,7 @@ var shows = [
   {
     "MainTitle": "Parks and Recreation",
     "EpisodeName": "Boys Club",
-    "Description": "As Andy cleans up the house to surprise Ann, Leslie tries to infiltrate the boys's club of politics by crashing an after-work gathering at Pawnee Town Hall. Trying to fit in, she opens a gift basket worth more than $25, breaking the local government ethics laws. She apologetically refers Pawnee officials to a website about the pit to highlight her accomplishments, but gets in more trouble when underage intern April Ludgate (Aubrey Plaza) posts a video of herself drinking on the site. Leslie is called before an ethics committee, but is let off with a warning after Ron, who despises the town's disciplinary process, defends her. Leslie is pleased when Mark tells her the warning officially makes her a member of the boys' club.",
+    "Description": "Leslie tries to infiltrate the boys' club of politics by crashing an after-work gathering at the town hall. Trying to fit in, she opens a gift basket breaking the local government ethics, and then is called before an ethics committee.",
     "AirTime": "10:00 pm",
     "DateofAir": "09-24-16",
     "Runtime": "30 minutes"
@@ -132,8 +131,8 @@ var shows = [
 
   {
     "MainTitle": "Game of Thrones",
-    "EpisodeName": "Garden of Thrones",
-    "Description": "Catelyn tries to convince the Baratheon brothers to abandon their quarrel and unite against the Lannisters, while Sansa is abused by Joffrey in revenge for her brother's victories. Tyrion intervenes and in return, Joffrey is cruel to the prostitutes Tyrion sends him as 'gifts'. Arya and Gendry are taken captives to the castle of Harrenhal. After her exhausting journey through the desert, Daenerys arrives at the gates of the prosperous city of Qarth. Melisandre gives birth to a shadow creature.",
+    "EpisodeName": "Garden of Bones",
+    "Description": "Lord Baelish arrives at Renly's camp just before he faces off against Stannis. Daenerys and her company are welcomed into the city of Qarth. Arya, Gendry, and Hot Pie find themselves imprisoned at Harrenhal.",
     "AirTime": "9:00 pm",
     "DateofAir": "09-24-16",
     "Runtime": "60 minutes"
